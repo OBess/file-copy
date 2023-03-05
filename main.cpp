f747fa92add7116c860b34908334632a0d56d8b7
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2023-03-05 19:54:14 +0200

    Add boost.program_options

--- main.cpp
+++ main.cpp
@@ -1,7 +1,57 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+
+#include <boost/program_options.hpp>
 
 int main(int argc, const char *argv[])
 {
-    std::cout << "Hello World!\n";
+    std::string in_filepath{};
+    std::string out_filepath{};
+
+    try
+    {
+        namespace po = boost::program_options;
+
+        po::options_description desc("Allowed options");
+
+        // clang-format off
+        desc.add_options()
+            ("help", "produce help message")
+            ("in", po::value<std::string>(&in_filepath), "the file to read")
+            ("out", po::value<std::string>(&out_filepath), "the file to write");
+        // clang-format on
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if (vm.count("help"))
+        {
+            std::cout << desc << "\n";
+            return 1;
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    if (in_filepath.empty())
+    {
+        std::cerr << "There is no file to read!\n";
+        return -1;
+    }
+    else if (out_filepath.empty())
+    {
+        std::cerr << "There is no file to write!\n";
+        return -1;
+    }
+
+    if (std::filesystem::exists(in_filepath) == false)
+    {
+        std::cerr << "This read file does not exist!\n";
+        return -1;
+    }
+
     return 0;
 }
