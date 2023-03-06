1551a6d330793e592152cce1a1313d8290eed7f4
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2023-03-06 20:05:57 +0200

    update

--- main.cpp
+++ main.cpp
@@ -13,12 +13,22 @@ class copy
 public:
     copy(const std::string &in_filepath, const std::string &out_filepath)
         : _inFile{in_filepath, std::ios_base::binary},
-          _outFile{out_filepath, std::ios_base::binary}
+          _outFile{out_filepath, std::ios_base::binary},
+          _sizeOfFile{std::filesystem::file_size(in_filepath)}
     {
     }
 
     void run()
     {
+        // char c{};
+        // _inFile.get(c);
+
+        // while (_inFile)
+        // {
+        //     _outFile << c;
+        //     _inFile.get(c);
+        // }
+
         [[maybe_unused]] std::jthread threadReader{[this]
                                                    {
                                                        asyncReadFile();
@@ -36,16 +46,12 @@ private:
     {
         while (_inFile)
         {
-            char symbol{};
-
             { // Read for first line of cache
-                std::lock_guard lk(_mtx1);
+                std::unique_lock lk(_mtx1);
 
-                _inFile.get(symbol);
-                for (size_t i = 0; i < _bufferLineSize && _inFile; ++i)
+                for (size_t i = 0; i < _bufferLineSize; ++i)
                 {
-                    _buffer[i] = symbol;
-                    _inFile.get(symbol);
+                    _inFile.get(_buffer1[i]);
 
                     _counter.fetch_add(1, std::memory_order_release);
                     ++readerCounter;
@@ -53,15 +59,11 @@ private:
             }
 
             { // Read for second line of cache
-                std::lock_guard lk(_mtx2);
+                std::unique_lock lk(_mtx2);
 
-                const size_t doubleSize = _bufferLineSize + _bufferLineSize;
-
-                _inFile.get(symbol);
-                for (size_t i = _bufferLineSize; i < doubleSize && _inFile; ++i)
+                for (size_t i = 0; i < _bufferLineSize; ++i)
                 {
-                    _buffer[i] = symbol;
-                    _inFile.get(symbol);
+                    _inFile.get(_buffer2[i]);
 
                     _counter.fetch_add(1, std::memory_order_release);
                     ++readerCounter;
@@ -72,32 +74,28 @@ private:
 
     void asyncWriteToFile()
     {
-        while (_counter.load(std::memory_order_acquire) == 0)
-        {
-        }
-
-        while (_counter.load(std::memory_order_acquire) > 0)
+        while (_sizeOfFile > 0)
         {
             { // Write first line of cache to file
-                std::lock_guard lk(_mtx1);
-                for (size_t i = 0; i < _bufferLineSize && _counter.load(std::memory_order_acquire) > 0; ++i)
-                {
-                    _outFile << _buffer[i];
+                std::unique_lock lk(_mtx1);
 
-                    _counter.fetch_sub(1, std::memory_order_release);
+                for (size_t i = 0; i < _bufferLineSize && _sizeOfFile > 0; ++i)
+                {
+                    _outFile << _buffer1[i];
+
+                    --_sizeOfFile;
                     ++writeCounter;
                 }
             }
 
             { // Write second line of cache to file
-                std::lock_guard lk(_mtx2);
+                std::unique_lock lk(_mtx2);
 
-                const size_t doubleSize = _bufferLineSize + _bufferLineSize;
-                for (size_t i = _bufferLineSize; i < doubleSize && _counter.load(std::memory_order_acquire) > 0; ++i)
+                for (size_t i = 0; i < _bufferLineSize && _sizeOfFile > 0; ++i)
                 {
-                    _outFile << _buffer[i];
+                    _outFile << _buffer2[i];
 
-                    _counter.fetch_sub(1, std::memory_order_release);
+                    --_sizeOfFile;
                     ++writeCounter;
                 }
             }
@@ -109,15 +107,16 @@ private:
     std::ofstream _outFile;
     std::mutex _mtx1, _mtx2;
 
-    bool _endOfRead{false};
-
     std::atomic<int64_t> _counter{0};
     size_t readerCounter{0};
     size_t writeCounter{0};
 
+    size_t _sizeOfFile{0};
+
     static constexpr size_t _bufferLineSize =
         std::hardware_destructive_interference_size;
-    char _buffer[_bufferLineSize + _bufferLineSize]{};
+    char _buffer1[_bufferLineSize]{};
+    char _buffer2[_bufferLineSize]{};
 };
 
 int main(int argc, const char *argv[])
