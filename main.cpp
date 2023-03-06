05dfb6fc014c10f0826e3b4425f9d586bbe69d1f
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2023-03-06 17:47:37 +0200

    Some impls

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <atomic>
+#include <barrier>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -12,15 +13,23 @@ class copy
 {
 public:
     copy(const std::string &in_filepath, const std::string &out_filepath)
-        : _inFile{in_filepath}, _outFile{out_filepath}
+        : _inFile{in_filepath, std::ios_base::binary},
+          _outFile{out_filepath, std::ios_base::binary}
     {
     }
 
     void run()
     {
-        std::jthread th1{[this]
-                         { asyncReadFile(); }};
-        asyncWriteToFile();
+        [[maybe_unused]] std::jthread threadReader{[this]
+                                                   {
+                                                       asyncReadFile();
+                                                       std::cout << "Read bytes: " << readerCounter << '\n';
+                                                   }};
+        [[maybe_unused]] std::jthread threadWriter{[this]
+                                                   {
+                                                       asyncWriteToFile();
+                                                       std::cout << "Wrote bytes: " << writeCounter << '\n';
+                                                   }};
     }
 
 private:
@@ -28,48 +37,60 @@ private:
     {
         while (_inFile)
         {
+            char symbol{};
+
             { // Read for first line of cache
                 std::lock_guard lk(_mtx1);
-                for (size_t i = 0; i < _bufferLineSize; ++i)
-                {
-                    char symbol{};
-                    _inFile >> std::noskipws >> symbol;
 
+                _inFile.get(symbol);
+                for (size_t i = 0; i < _bufferLineSize && _inFile; ++i)
+                {
                     _buffer[i] = symbol;
+                    _inFile.get(symbol);
+
+                    _counter.fetch_add(1, std::memory_order_release);
+                    ++readerCounter;
                 }
             }
+
+            _phase.fetch_add(1, std::memory_order_release);
 
             { // Read for second line of cache
                 std::lock_guard lk(_mtx2);
 
                 const size_t doubleSize = _bufferLineSize + _bufferLineSize;
-                for (size_t i = _bufferLineSize; i < doubleSize; ++i)
-                {
-                    char symbol{};
-                    _inFile >> std::noskipws >> symbol;
 
+                _inFile.get(symbol);
+                for (size_t i = _bufferLineSize; i < doubleSize && _inFile; ++i)
+                {
                     _buffer[i] = symbol;
+                    _inFile.get(symbol);
+
+                    _counter.fetch_add(1, std::memory_order_release);
+                    ++readerCounter;
                 }
             }
-        }
 
-        _write = false;
+            _phase.fetch_add(1, std::memory_order_release);
+        }
     }
 
     void asyncWriteToFile()
     {
-        while (_write)
+        while (_counter.load(std::memory_order_acquire) == 0 && _phase.load(std::memory_order_acquire) == 0)
+        {
+        }
+
+        while (_phase.load(std::memory_order_acquire) > 0)
         {
             { // Write first line of cache to file
                 std::lock_guard lk(_mtx1);
-                for (size_t i = 0; i < _bufferLineSize; ++i)
+                for (size_t i = 0; i < _bufferLineSize && _counter.load(std::memory_order_acquire) > 0; ++i)
                 {
-                    if (_buffer[i] == '\0')
-                    {
-                        break;
-                    }
-
                     _outFile << _buffer[i];
+
+                    _counter.fetch_sub(1, std::memory_order_release);
+                    ++writeCounter;
                 }
             }
 
@@ -77,14 +98,12 @@ private:
                 std::lock_guard lk(_mtx2);
 
                 const size_t doubleSize = _bufferLineSize + _bufferLineSize;
-                for (size_t i = _bufferLineSize; i < doubleSize; ++i)
+                for (size_t i = _bufferLineSize; i < doubleSize && _counter.load(std::memory_order_acquire) > 0; ++i)
                 {
-                    if (_buffer[i] == '\0')
-                    {
-                        break;
-                    }
-
                     _outFile << _buffer[i];
+
+                    _counter.fetch_sub(1, std::memory_order_release);
+                    ++writeCounter;
                 }
             }
         }
@@ -95,7 +114,14 @@ private:
     std::ofstream _outFile;
     std::mutex _mtx1, _mtx2;
 
-    bool _write{true};
+    bool _endOfRead{false};
+
+    std::atomic<int64_t> _counter{0};
+    std::atomic<int64_t> _phase{0};
+    size_t readerCounter{0};
+    size_t writeCounter{0};
+
+    std::barrier _bar;
 
     static constexpr size_t _bufferLineSize =
         std::hardware_destructive_interference_size;
@@ -134,22 +160,25 @@ int main(int argc, const char *argv[])
         std::cerr << e.what() << '\n';
     }
 
-    if (in_filepath.empty())
-    {
-        std::cerr << "There is no file to read!\n";
-        return -1;
-    }
-    else if (out_filepath.empty())
-    {
-        std::cerr << "There is no file to write!\n";
-        return -1;
-    }
+    // if (in_filepath.empty())
+    // {
+    //     std::cerr << "There is no file to read!\n";
+    //     return -1;
+    // }
+    // else if (out_filepath.empty())
+    // {
+    //     std::cerr << "There is no file to write!\n";
+    //     return -1;
+    // }
 
-    if (std::filesystem::exists(in_filepath) == false)
-    {
-        std::cerr << "This read file does not exist!\n";
-        return -1;
-    }
+    // if (std::filesystem::exists(in_filepath) == false)
+    // {
+    //     std::cerr << "This read file does not exist!\n";
+    //     return -1;
+    // }
+
+    in_filepath = "C:\\My\\Projects\\cpp\\file-copy\\deploy\\Debug\\lab 1.docx";
+    out_filepath = "C:\\My\\Projects\\cpp\\file-copy\\deploy\\Debug\\output.docx";
 
     copy copyInstance{in_filepath, out_filepath};
     copyInstance.run();
